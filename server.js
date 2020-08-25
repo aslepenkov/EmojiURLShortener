68f35b619831bb7b9a46f79961f81b72dda5d288
aslepenkov <alexmichel249@gmail.com>
2020-08-25 22:25:17 +0700

    add emojis

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const cluster = require('cluster');
 const os = require('os');
-const pid = process.pid;
+
+const { pid } = process;
 
 if (cluster.isMaster) {
-    const cpuCount = os.cpus().length;
-    console.log(`CPUs: ${cpuCount}`);
-    console.log(`Master started. PID: ${pid}`);
-    for (let i = 0; i < cpuCount - 1; i++) {
-        const worker = cluster.fork();
-        worker.on('exit', () => {
-            console.log(`Worker ${worker.process.pid} died`);
-            cluster.fork();
-        });
-        worker.send('Hello from server');
-        worker.on('message', (msg) => {
-            console.log(`Message from worker ${worker.process.pid} : ${JSON.stringify(msg)}`)
-        });
-    }
-
-}
-
-else if (cluster.isWorker) {
-    require(`./worker.js`);
-    process.on('message', (msg) => {
-        console.log(`[!]Message from master: ${msg}`);
+  const cpuCount = os.cpus().length;
+  console.log(`CPUs: ${cpuCount}`);
+  console.log(`Master started. PID: ${pid}`);
+  for (let i = 0; i < cpuCount - 1; i += 1) {
+    const worker = cluster.fork();
+    worker.on('exit', () => {
+      console.log(`Worker ${worker.process.pid} died`);
+      cluster.fork();
     });
-    //process.send({ message: `Hello!`, pid })
+    worker.send('Hello from server');
+    worker.on('message', (msg) => {
+      console.log(`Message from worker ${worker.process.pid} : ${JSON.stringify(msg)}`);
+    });
+  }
+} else if (cluster.isWorker) {
+  require('./worker.js');
+  process.on('message', (msg) => {
+    console.log(`[!]Message from master: ${msg}`);
+  });
+  // process.send({ message: `Hello!`, pid })
 }
--- worker.js
+++ worker.js
@@ -4,70 +4,90 @@ const cors = require('cors');
 const morgan = require('morgan');
 const yup = require('yup');
 const monk = require('monk');
+const getEmojis = require('./emojis.js');
 const { nanoid } = require('nanoid');
 
 require('dotenv').config();
 
-const db = monk(process.env.MONGO_URI);
+const db = monk(process.env.MONGODB_URI);
 const urls = db.get('urls');
-urls.createIndex('name');
+urls.createIndex({ slug: 1 }, { unique: true });
 
 const app = express();
 
-app.use(helmet()); //Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
-app.use(morgan('dev')); //HTTP request logger middleware for node.js
-app.use(cors()); //CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
-app.use(express.json()); //is a method inbuilt in express to recognize the incoming Request Object as a JSON Object.
+app.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP headers
+app.use(morgan('dev')); // HTTP request logger middleware for node.js
+app.use(cors()); // for providing CORS with various options.
+app.use(express.json()); //  recognize the incoming Request Object as a JSON Object.
 app.use(express.static('./public'));
 
-app.get('/:id', (req, res) => {
-
+app.get('/:id', async (req, res) => {
+  const { id: slug } = req.params;
+  try {
+    const urlObj = await urls.findOne({ slug });
+    if (urlObj) {
+      res.redirect(urlObj.url);
+    }
+    res.redirect(`/?error=${slug} not found`);
+  } catch (error) {
+    res.redirect('/?error=Link not found');
+  }
 });
 
-app.get('/url/:id', (req, res) => {
-
-});
+app.get('/url/:id', (req, res) => {});
 
 const scheme = yup.object().shape({
-    slug: yup.string().trim().matches(/[\w\-]/i),
-    url: yup.string().trim().url().required(),
-});//Yup is a JavaScript schema builder for value parsing and validation. Define a schema, transform a value to match, validate the shape of an existing value, or both. Yup schema are extremely expressive and allow modeling complex, interdependent validations, or value transformations
+  slug: yup.string().trim().matches(/[\w-]/i),
+  url: yup.string().trim().url().required(),
+}); // Yup is a JavaScript schema builder for value parsing and validation
 
 app.post('/url', async (req, res, next) => {
-    let { slug, url } = req.body;
-    try {
+  let { slug, url } = req.body;
+  try {
+    await scheme.validate({
+      slug,
+      url,
+    });
 
-        await scheme.validate({
-            slug, url
-        });
-
-        if (!slug) {
-            slug = nanoid(5);//A tiny, secure, URL-friendly, unique string ID generator for JavaScript
-        }
-        slug = slug.toLowerCase();
-        res.json({
-            slug, url
-        });
-    } catch (error) {
-        next(error);
+    if (!slug) {
+      const id = getEmojis.getEmojis(5);
+      console.log(id);
+      slug = nanoid(5); // A tiny, secure, URL-friendly, unique string ID generator for JavaScript
+    } else {
+      const isExisting = await urls.findOne({ slug });
+      if (isExisting) {
+        throw new Error('Slug isn\'t unique!');
+      }
     }
+    slug = slug.toLowerCase();
+    const secret = nanoid(10).toLowerCase();
+    const newUrl = {
+      url,
+      slug,
+      secret,
+    };
+    const created = await urls.insert(newUrl);
+    res.json(created);
+  } catch (error) {
+    next(error);
+  }
 });
 app.use((error, req, res, next) => {
-    if (error.status) {
-        res.status(error.status)
-    } else {
-        res.status(500);
-    }
-    res.json({
-        message: error.message,
-        stack: process.env.NODE_ENV === 'production' ? '☺' : error.stack
-    });
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '☺' : error.stack,
+  });
 });
 
-const pid = process.pid;
+const { pid } = process;
 const port = process.env.PORT || 8001;
 app.listen(port, () => {
-    console.log(`Server started. PID: ${pid}  PORT: ${port}`)
+  console.log(`Server started. PID: ${pid}  PORT: ${port}`);
 });
 // http.createServer((req, res) => {
 
