478ec93aa3425233f33e7f5e50bb15347194fe87
aslepenkov <alexmichel249@gmail.com>
2020-08-25 15:23:30 +0700

    upd to URL SHORTENER

--- worker.js
+++ worker.js
@@ -1,18 +1,38 @@
-const http = require('http');
-const pid = process.pid;
-const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const morgan = require('morgan');
+
 const port = process.env.PORT || 8001;
-require('dotenv').config(); 
+const pid = process.pid;
+
+require('dotenv').config();
+
+const app = express();
 
 
-http.createServer((req, res) => {
+app.use(helmet()); //Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
+app.use(morgan('dev')); //HTTP request logger middleware for node.js
+app.use(cors()); //CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
+app.use(express.json()); //is a method inbuilt in express to recognize the incoming Request Object as a JSON Object.
 
-    //ANY HEAVY TASK
-    for (let i = 0; i < 1e7; i++) { }
 
-    res.end(`Server created\n`)
-}).listen(port, () => {
+app.get('/:id', (req, res) => {
+    res.json({
+        message: 'MSG'
+    })
+});
+
+app.listen(port, () => {
+
     console.log(`Server started. PID: ${pid}  PORT: ${port}`)
-})
+});
+// http.createServer((req, res) => {
+
+//     //ANY HEAVY TASK
+//     for (let i = 0; i < 1e7; i++) { }
+
+//     res.end(`Server created\n`)
+// }).listen(port, () => {
+//     console.log(`Server started. PID: ${pid}  PORT: ${port}`)
+// })
